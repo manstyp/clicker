950f1e6fcf3dea58fa3d252dcea6dabd0b864603
manstyp <115995992+manstyp@users.noreply.github.com>
2024-04-22 12:29:06 +0000

    rah

--- api/index.js
+++ api/index.js
@@ -1,17 +1,28 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3003;
 const path = require("path");
 const viewsDirectory = path.join(process.cwd(), "views");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const User = require("./models/user");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
 //MIDDLEWARE
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static("public"));
 app.set("views", viewsDirectory);
 app.set("view engine", "pug");
+app.use(
+  session({
+    secret: "hej",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 600000 },
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri =
   "mongodb+srv://mankri752:userPassword!!!@clicker.2uzk8ms.mongodb.net/?retryWrites=true&w=majority";
@@ -33,17 +44,85 @@ app.get("/", (req, res) => {
 });
 
 app.get("/game", (req, res) => {
-  res.render("game", {});
+  res.render("game", {
+    username: req.session.username,
+  });
 });
 
-app.get("/home", (req, res) => {
-  res.render("home");
+app.get("/home", async (req, res) => {
+  if (!req.session.userId && process.env.ENVIRONMENT != "DEV") {
+    res.render("home");
+  } else {
+    let userId, user;
+    if (process.env.ENVIRONMENT == "DEV") {
+      userId = 0;
+      user = {
+        username: "Admin",
+      };
+    } else {
+      userId = req.session.userId;
+      user = await User.findById(userId);
+    }
+
+    res.redirect("/game");
+  }
 });
 
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("register", (req, res) => {
+app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/authenticate-register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).send("Username is already taken");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, password: hashedPassword });
+
+    await newUser.save();
+
+    req.session.userId = newUser._id;
+    // req.session.username = newUser.username;
+    // req.session.password = newUser.password;
+
+    res.status(201).redirect("/login");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/authenticate-login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      req.session.userId = user._id;
+      res.status(201).redirect("/home");
+    } else {
+      res.status(401).send("Invalid password");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+
+  req.session.username = username;
 });
